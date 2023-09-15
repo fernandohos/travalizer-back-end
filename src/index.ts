5086e2ff2013ceadad66bd27d39d65ea2a29b786
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-09-14 21:03:14 -0300

    :construction: add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "@routes/auth";
+import { connectDb } from "@config/db";
+import mongoose from "mongoose";
 
 dotenv.config();
 const PORT = process.env.PORT || 3030;
+connectDb();
 
 const app = express();
 
+app.use(authRouter);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to MongoDB");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
-})
+});
