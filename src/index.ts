8e5379056e8f3f358edaab1d051de1b80329b5a1
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-09-24 10:53:02 -0300

    :construction: add cors config

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRouter from "@routes/auth";
 import { connectDb } from "@config/db";
 import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 3030;
@@ -10,6 +11,7 @@ connectDb();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(authRouter);
 
